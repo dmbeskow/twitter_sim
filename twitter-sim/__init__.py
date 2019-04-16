cf25435137baee572aba51f7f892a51f26055056
David Beskow <dbeskow@GS17928.SP.CS.CMU.EDU>
2019-04-16 09:59:41 -0400

    added dynamic network and reply functionality

--- twitter-sim/__init__.py
+++ twitter-sim/__init__.py
@@ -14,23 +14,41 @@ import numpy as np
 from numpy.random import choice
 from collections import Counter
 from sklearn.metrics import pairwise_distances
-
+import progressbar
 
 #%%
 def scale(x):
     x = np.array(x)
     return(x/np.max(x))
+    
 #%%
-size = 20
-prob = 0.3
-influence_proportion = 0.1
-bucket = [0,1]
-
-#G = nx.erdos_renyi_graph(size, prob, seed=767, directed=True)
-G = nx.scale_free_graph(size)
-A = nx.adjacency_matrix(G).astype(bool)
-similarity = 1 - pairwise_distances(A.todense(), metric = 'jaccard')
-influence = scale(list(dict(G.degree()).values()))
+def link_prediction(G, node):
+    potential = []
+    successors = G.successors(node)
+    predecessors = list(G.predecessors(node)) 
+    for successor in successors:
+        friends = G.predecessors(successor)
+        for friend in friends:
+            if friend != node:
+                potential.append(friend)
+    final = []
+    if len(potential) > 0:
+        jaccard1 = similarity[node,potential]
+        i = np.argmax(jaccard1)
+        link = (node,potential[i])
+        if ~G.has_edge(link[0],link[1]):
+            final.append(link)
+    elif len(predecessors) > 0:
+        get_one = random.sample(list(predecessors),1)
+        link = (node,get_one[0])
+        if ~G.has_edge(link[0],link[1]):
+            final.append(link)
+#    else:
+#        all_nodes = list(G.nodes)
+#        all_nodes.remove(node)
+#        get_one = random.sample(all_nodes,1)
+#        final.append((node,get_one[0]))
+    return(final)
 #%%
 def draw_simulation(graph, save = False):
     nodes = graph.nodes()
@@ -41,7 +59,38 @@ def draw_simulation(graph, save = False):
                                 with_labels=False, node_size=100, cmap='YlOrRd')
     plt.colorbar(nc)
     plt.axis('off')
-    plt.show()
+    plt.show()    
+#%%
+size = 200
+prob = 0.3
+influence_proportion = 0.1
+bucket1 = [0,1]
+bucket2 = [0,-1]
+probability_of_link = 0.1
+dynamic_network = True
+global_perception = 0.001
+similarity_weight = 3
+prestige_weight = 1
+
+#G = nx.erdos_renyi_graph(size, prob, seed=767, directed=True)
+G = nx.scale_free_graph(size)
+
+## Remove self_loops and isololates
+G.remove_edges_from(list(G.selfloop_edges()))
+G.remove_nodes_from(list(nx.isolates(G)))
+
+A = nx.adjacency_matrix(G).astype(bool)
+b = np.squeeze(np.asarray(A.sum(axis = 1)))
+b = np.argwhere(b==0)
+for node in b:
+    connected = [to for (fr, to) in G.edges(node)]
+    unconnected = [n for n in G.nodes() if not n in connected] 
+    new = random.sample(unconnected,1)
+    G.add_edge(node[0], new[0])
+
+A = nx.adjacency_matrix(G).astype(bool)
+similarity = 1 - pairwise_distances(A.todense(), metric = 'jaccard')
+prestige = scale(list(dict(G.degree()).values()))
 #%%
 
 for node, data in G.nodes(data=True):
@@ -49,29 +98,33 @@ for node, data in G.nodes(data=True):
     data['wake'] = 0 + np.round(np.random.exponential(scale = 1 / data['lambda']))
     data['inbox'] = []
     data['belief'] = np.random.uniform(0.0,1.0)
-    data['susceptibility'] = np.random.uniform(0,1)
+    data['kind'] = 'normal'
+#    data['susceptibility'] = np.random.uniform(0,1)
     
-list(G.nodes(data=True))
+#list(G.nodes(data=True))
 draw_simulation(G)
+
+#list(G.selfloop_edges())
 #%%
 total_tweets = []
 all_beliefs = {'time':[],'user':[],'beliefs':[]}
-global_perception = 0.0001
-for step in range(3000):
-    # Once a week we update the similarity matrix and Global Perception 
+bar = progressbar.ProgressBar()
+for step in bar(range(2000)):
+    # Once a week we update the similarity matrix and Global Perception and prestige
     if (step % 168) == 0:
         A = nx.adjacency_matrix(G).astype(bool)
         similarity = 1 - pairwise_distances(A.todense(), metric = 'jaccard')
+        prestige = scale(list(dict(G.degree()).values()))
         
         ## Update Global Perception
         if len(total_tweets) > 0:
             df = pd.concat(total_tweets)
-            global_perception = df['tweets'].mean()
+            global_perception = 0.001*df['tweets'].mean()
     for node, data in G.nodes(data=True):
         all_beliefs['time'].append(step)
         all_beliefs['user'].append(node);
         all_beliefs['beliefs'].append(data['belief']);
-        if data['wake'] == step:
+        if data['wake'] < step:
             # Get new 'wake' time
             data['wake'] = data['wake'] + np.round(np.random.exponential(scale = 1 / data['lambda']))
             # Read Tweets
@@ -79,45 +132,40 @@ for step in range(3000):
                 number_to_read = min(random.randint(0,20),len(data['inbox']))
                 read_tweets = data['inbox'][-number_to_read:]
                 perc = np.mean(read_tweets)
-                #update influence
+                #update prestige
 #                data['belief'] = data['belief'] + data['susceptibility'] * perc * (1-data['belief'])
-                new_belief = data['belief'] +   (perc + global_perception) * (1-data['belief'])
-                data['belief'] = new_belief
-                
-            # Send Tweets
-            chance = data['belief'] * influence_proportion
-            tweets = list(choice(bucket, np.random.randint(0,10),p=[1-chance, chance]))
+                if perc > 0:
+                    new_belief = data['belief'] +   (perc + global_perception) * (1-data['belief'])
+                else:
+                    new_belief = data['belief'] +   (perc + global_perception) * (data['belief'])
+                data['belief'] = new_belief     
+            # Send Tweets for bots
+            if data['kind'] == 'bot':
+                chance = 0.8
+                tweets = list(choice(bucket1, np.random.randint(0,10),p=[1-chance, chance]))
+            # Send Tweets for normal users
+            elif data['belief'] > 0.05:
+                chance = data['belief'] * influence_proportion
+                tweets = list(choice(bucket1, np.random.randint(0,10),p=[1-chance, chance]))
+            # Send Tweets for Beacons
+            else:
+                chance = 0.8
+                tweets = list(choice(bucket2, np.random.randint(0,10),p=[1-chance, chance]))
             total_tweets.append(pd.DataFrame({'tweets': tweets, 'time' :[step] * len(tweets)}))
             predecessors = G.predecessors(node)
             for follower in predecessors:
-                homophily = similarity[node,follower]
-                importance = influence[follower]
+                homophily = similarity_weight * similarity[node,follower]
+                importance = prestige_weight * prestige[follower]
                 tweets = [homophily * importance * i for i in tweets]
                 G.nodes[follower]['inbox'] = G.nodes[follower]['inbox'] + tweets
-#list(G.nodes(data=True))
+                
+            # If probabliliy right, add link
+            if (np.random.uniform(0,1) < probability_of_link) and (dynamic_network):
+                new_link = link_prediction(G,node)
+                if len(new_link) > 0:
+                    G.add_edges_from(new_link)                
+#x = list(G.nodes(data=True))
 draw_simulation(G)
-
-
-#%%
-def draw_tweet_timeline(t_tweets, plot_type = 'area'):
-    df = pd.concat(total_tweets)    
-    df['type'] = 'noise'
-    df.loc[df['tweets'] != 0,['type']] = 'disinformation'
-    if plot_type == 'area':
-        df.groupby(['time','type']).sum().unstack().plot.area()
-    else:
-        df.groupby(['time']).sum().plot(title = 'Tweets Per Hour')
-draw_tweet_timeline(total_tweets)
-#%%
-def draw_tweet_bar(t_tweets):
-    df = pd.concat(total_tweets)    
-    df['type'] = 'noise'
-    df.loc[df['tweets'] != 0,['type']] = 'disinformation'
-    df = df['type'].value_counts()
-    ax = df.plot(kind = 'bar', title = 'Tweets by Type',color = ['blue','red'], alpha = 0.6)
-    for x,y in enumerate(df):
-        ax.text(x,y,y)
-draw_tweet_bar(total_tweets)
 #%%
 def draw_beliefs(all_beliefs, breaks = 'weeks'):
     df = pd.DataFrame(all_beliefs)  
@@ -145,10 +193,31 @@ def draw_beliefs(all_beliefs, breaks = 'weeks'):
 
 draw_beliefs(all_beliefs)
 #%%
+def draw_tweet_timeline(t_tweets, plot_type = 'area'):
+    df = pd.concat(total_tweets)    
+    df['type'] = 'noise'
+    df.loc[df['tweets'] != 0,['type']] = 'disinformation'
+    if plot_type == 'area':
+        df.groupby(['time','type']).sum().unstack().plot.area()
+    else:
+        df.groupby(['time']).sum().plot(title = 'Tweets Per Hour')
+draw_tweet_timeline(total_tweets)
+#%%
+def draw_tweet_bar(t_tweets):
+    df = pd.concat(total_tweets)    
+    df['type'] = 'noise'
+    df.loc[df['tweets'] != 0,['type']] = 'disinformation'
+    df = df['type'].value_counts()
+    ax = df.plot(kind = 'bar', title = 'Tweets by Type',color = ['blue','red'], alpha = 0.6)
+    for x,y in enumerate(df):
+        ax.text(x,y,y)
+draw_tweet_bar(total_tweets)
+
+#%%
 from scipy.spatial import distance
 distance.jaccard([0, 0], [0, 1, 0])    
     
-nx.draw(G)
+nx.draw(G2, with_labels = True)
     
 G.add_node(1, time='5pm')
 G.nodes[0]['foo'] = 'bar'
@@ -168,3 +237,10 @@ df = pd.DataFrame(np.random.rand(10, 4), columns=['a', 'b', 'c', 'd'])
  
 # plot
 df.plot.area()
+
+
+G = nx.complete_graph(5)
+G2 = nx.Graph(G.to_undirected())
+preds = nx.adamic_adar_index(G2.to_undirected())
+for u, v, p in preds:
+    print('(%d, %d) -> %.8f' % (u, v, p))
